217196542d229cccd147aef388cb2edfd7c053a0
john-wayne <anvar.kartakaev@gmail.com>
2025-02-12 21:06:59 +0500

    Commit starting project

---
+++ src/main/java/org/skypro/exam_question_generator/ExamQuestionGeneratorApplication.java
@@ -0,0 +1,13 @@
+package org.skypro.exam_question_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQuestionGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQuestionGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/skypro/exam_question_generator/ExamQuestionGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.skypro.exam_question_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamQuestionGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
